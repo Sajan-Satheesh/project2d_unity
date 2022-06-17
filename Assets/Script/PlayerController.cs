eddea045ace9e2f3d1e88ade8d814d4dda93d991
Sajan Satheesh <64242770+Sajan-Satheesh@users.noreply.github.com>
2022-06-17 18:01:49 +0530

    Gameupdate_2
    1.Included pause, resume and play again function on click.
    2. Included enemies in the game who trigger lost function on collision.
    
    lack of implementation:
    1. quit and screen pause
    2. scoring system
    3.enemy movement
    4. different levels

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D Player;
     public GameObject Game_Won_Panel;
     public GameObject Pause_Panel;
+    public GameObject Game_Lost_Panel;
 
     public float speed;
-    public bool gamewon;
+    public bool gameover;
     public bool pause;
     // Start is called before the first frame update
     void Start()
@@ -18,14 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gamewon || pause)
+        if (gameover)
         {
             return;
         }
         if (Input.GetAxis("Cancel") > 0)
         {
             Pause_Panel.SetActive(true);
-            pause = true;
         }
         if (Input.GetAxis("Horizontal") > 0)
         {
@@ -54,14 +55,30 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Level Completed!");
             Game_Won_Panel.SetActive(true);
-            gamewon = true;
+            gameover = true;
         }
-        
+        if (collision.CompareTag("enemy"))
+        {
+            Debug.Log("You Lost!");
+            Game_Lost_Panel.SetActive(true);
+            gameover = true;
+        }
+
         if (collision.CompareTag("item"))
         {
             Debug.Log("item collected!");
         }
            
     }
+    public void click_playagain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("play again");
+    }
+    public void click_resume()
+    {
+        Pause_Panel.SetActive(false);
+    }
+
 
 }
